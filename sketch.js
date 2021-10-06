55587c7adcf79a9060bcb84137e11b1d2601a59b
Chris Kubick <socalledsound@gmail.com>
2021-10-06 09:47:13 -0700

    init

--- sketch.js
+++ sketch.js
@@ -1,18 +1,34 @@
 // constants to set the canvas size to the size of the window
 const canvasWidth = window.innerWidth
 const canvasHeight = window.innerHeight
-// variables for the ball
-let ballX = canvasWidth/2
-let ballY = canvasHeight/2
-let ballSize = canvasWidth/30
-let ballColor = [220, 0, 220]
-let ballSpeed = 10
-// variables for the goal
-let goalSize = canvasWidth/20
-let goalX = canvasWidth - canvasWidth/10
-let goalY = canvasHeight/2 - goalSize
-let goalColor = [220,220,0]
+// i changed these variable names
+// and also changed them where we call the functions
+// variables for mario
+let marioX = canvasWidth/2
+let marioY = canvasHeight/2
+let marioWidth = canvasWidth/30
+let marioHeight = canvasWidth/15
+let marioSpeed = 20
+// variables for the coin
+let coinWidth = canvasWidth/30
+let coinHeight = canvasWidth/15
+let coinX = canvasWidth - canvasWidth/10
+let coinY = canvasHeight/2
+let showCoin = true
+let score = 0
+let coinsCollected = [false]
 
+// now lets load in some assets
+let marioImg, coinImg
+
+// HEY! notice that to use sounds we have to add the p5.sound library to libs
+// and then import it in our index.html
+
+function preload(){
+    marioImg = loadImage('./assets/mario.png')
+    coinImg = loadImage('./assets/coin.png')
+    coinSound = loadSound('./assets/coin.mp3')
+}
 
 function setup(){
     createCanvas(canvasWidth, canvasHeight)
@@ -21,36 +37,85 @@ function setup(){
 
 function draw(){
     background(30)
+
+    //let's also draw a scoreboard
+    drawScoreboard(score)
+
     // now we have some functions (below) that draw our characters!
-    drawGoal(goalX, goalY, goalSize, goalColor)
-    drawCharacter(ballX, ballY, ballSize, ballColor)
+    // instead of a color let's pass in an image
+    drawMario(marioImg, marioX, marioY, marioWidth, marioHeight)
+    if(!coinsCollected[0]){
+        drawCoin(coinImg, coinX, coinY, coinWidth, coinHeight)
+    }
+    
+    // every time through the draw loop let's check if mario made it to the coin
+    let atGoal = checkGoal(marioX, marioY, marioWidth, marioHeight, coinX, coinY, coinWidth, coinHeight)
+
+    if(atGoal){
+     // now we need to add an extra step to this 
+    // because we will stay at the goal.
+    // let's keep track of whether the coin has been collected previously
+    // I'm going to use an array, I bet you can guess why?
+    // we'll find out in the next version of this
+        if(!coinsCollected[0]){
+
+            coinsCollected[0]  =true
+            score++
+            coinSound.play()
+        }
 
 
-    // can you make a function that checks if the ball has reached its target????
-
+    }
 
 }
 
 function keyPressed(){
     console.log(key)
     if(key === 'd'){
-        ballX+=ballSpeed
+        marioX+=marioSpeed
     }
-    // now make it go other directions when other keys are pressed
+    if(key === 'a'){
+        marioX-=marioSpeed
+    }
+    if(key === 'w'){
+        marioY-=marioSpeed
+    }
+    if(key === 's'){
+        marioY+=marioSpeed
+    }
+    
 }
 
-// notice how we define the parameters and then use them in the function
-// so the names are different than they are when we call the function in our draw loop
-// we can call the parameters anything we want but we have to use the right parameter
-// names in our function body
-function drawGoal(x, y, size, col){
-    fill(col)
-    rect(x,y, size, size * 2)
+// new draw functions now, they use an image instead
+// we could probably write just one but I like the way it looks in the draw loop better this way personally
+// and later if we want to change the way we draw this we can
+function drawMario(img, x, y, w, h){
+    image(img, x, y, w, h)
 }
 
-function drawCharacter(x, y, size, col){
-    fill(col)
-    ellipse(x, y, size)
+function drawCoin(img, x, y, w, h){
+    image(img, x, y, w, h)
+}
+
+// notice how again I used different parameter names rather than the specific ones in the main program
+// that way we can use this function again in another project if we want to
+function checkGoal(x1,y1,w1, h1, x2, y2, w2, h2){
+    if(x1 + w1 > x2 &&
+        x1 < x2 + w2 && 
+        y1 < y2 + h2 &&
+        y1 + h1 > y2  ){
+            return true
+        } else {
+            return false
+        }
+}
+
+
+function drawScoreboard(score){
+    fill(255)
+    textSize(30)
+    text('score : ', 40, 40)
+    text(score, 140, 42)
 }
 
 
